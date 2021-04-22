05b1cb55e049c649ae040272ca7763401f07490d
EtienneBrJ <etiennebrxv@gmail.com>
2021-04-22 02:52:04 +0200

    add python-binance

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import websocket, json, pprint, talib, numpy
+import config
+from binance.client import Client
+from binance.enums import *
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
@@ -6,10 +9,22 @@ RSI_PERIOD = 14
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD = 30
 TRADE_SYMBOL = 'ETHUSDT'
-TRADE_QUANTITY = 0.05
+TRADE_QUANTITY = 0.005
 
 closes = []
 in_position = False
+
+client = Client(config.API_KEY, config.API_SECRET, tld='us')
+
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+    try:
+        print("sending order")
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        print(order)
+    except Exception as e:
+        return False
+
+    return True
 
 def on_open(ws):
     print('open connection')
@@ -19,7 +34,7 @@ def on_close(ws):
 
 def on_message(ws, message):
     global closes
-    print('received message')
+    #print('received message')
     json_message = json.loads(message)
     #pprint.pprint(json_message)
 
@@ -46,6 +61,9 @@ def on_message(ws, message):
                 if in_position:
                     print("Sell!  Sell!")
                     #binance shit
+                    order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
+                    if order_succeeded:
+                        in_position = False
                 else:
                     print("Overbought, dont own any, nothing to do")
 
@@ -55,7 +73,10 @@ def on_message(ws, message):
                 else:
                     print("Buy!! Buy!!")
                     #binance shit
-
+                    order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
+                    if order_succeeded:
+                        in_position = True
+                
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
